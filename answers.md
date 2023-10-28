66ce75111688d0a07da9fee398e29d9efedc0fb7
amaraM76 <136095008+amaraM76@users.noreply.github.com>
2023-10-27 21:21:44 -0400

    assignment3

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Assignment 3
 ## Answers
 
-**Name:**_________________________
+**Name:**__Amara Midouhas_______________________
 
 
 Place all written answers from `assignment-03.md` here for easier grading.
@@ -12,14 +12,14 @@ Place all written answers from `assignment-03.md` here for easier grading.
 
 
 - **b.**
+Since we are iterating through the characters in a list, the work depends on the size of the list to the work is constant so this would be O(n). For span, there are nested loops where the first loop goes through a list of strings and then the characters in that string, so there is a dependency. Also since we have a count to keep track of the paranthesis, there cant be parrel processing. Since each process of characters depends on the one before, the length or amount of characters is the span which is O(n)
+
+
+
+- **d.** For work, since the mapping is depenndent on the size of the list, it will take O(n). The Scan operation is also O(n) because it is applying f to the pair of elements in a so it depends on the length of the list. Since we are combining elements together and then cutting the size of the list in half and also creating a new list, it is O(log n).
 
 
 
 
-- **d.**
 
-
-
-
-
-- **f.**
+- **f.** Since we are splitting the list in two and having two parrel recursive calls it would be W(n)=2*W(n). Then to merge the left and right it will always be a fixed amount of operations which is O(1), so it will be O(n) . Then for span, since we are recursively dividing the list, it is log n. Therefore, it will be O(log n)
--- main.py
+++ main.py
@@ -27,6 +27,21 @@ def reduce(f, id_, a):
 
 #### Iterative solution
 def parens_match_iterative(mylist):
+    def parens_update(current_output, next_input):
+        if next_input == '(':
+            return current_output + 1
+        elif next_input == ')':
+            return current_output - 1
+        return current_output
+
+    counter = 0
+    for string in mylist:
+        for char in string:
+            counter = parens_update(counter, char)
+            if counter < 0:
+                return False
+
+    return counter == 0
     """
     Implement the iterative solution to the parens matching problem.
     This function should call `iterate` using the `parens_update` function.
@@ -71,6 +86,24 @@ def test_parens_match_iterative():
 #### Scan solution
 
 def parens_match_scan(mylist):
+    def paren_map(x):
+        if x == '(':
+            return 1
+        elif x == ')':
+            return -1
+        else:
+            return 0
+
+    def min_f(x, y):
+        if x < y:
+            return x
+        return y
+
+    mapped_list = list(map(paren_map, mylist))
+    result = scan(min_f, 0, mapped_list)
+    return result[-1] == 0
+
+
     """
     Implement a solution to the parens matching problem using `scan`.
     This function should make one call each to `scan`, `map`, and `reduce`
@@ -91,16 +124,11 @@ def parens_match_scan(mylist):
     pass
 
 def scan(f, id_, a):
-    """
-    This is a horribly inefficient implementation of scan
-    only to understand what it does.
-    We saw a more efficient version in class. You can assume
-    the more efficient version is used for analyzing work/span.
-    """
     return (
-            [reduce(f, id_, a[:i+1]) for i in range(len(a))],
-             reduce(f, id_, a)
-           )
+        [reduce(f, id_, a[:i + 1]) for i in range(len(a))],
+        reduce(f, id_, a) 
+    )
+
 
 def paren_map(x):
     """
@@ -152,6 +180,28 @@ def parens_match_dc(mylist):
     return n_unmatched_left==0 and n_unmatched_right==0
 
 def parens_match_dc_helper(mylist):
+    def merge(left, right):
+        unmatched_right = max(right[0] - left[1], 0)
+        unmatched_left = max(left[0] - right[1], 0)
+        return (unmatched_right, unmatched_left)
+
+    if len(mylist) == 0:
+        return (0, 0)
+
+    if len(mylist) == 1:
+        if mylist[0] == '(':
+            return (1, 0)
+        else:
+            return (0, 1)
+
+    midlength = len(mylist) // 2
+    left = mylist[:midlength]
+    right = mylist[midlength:]
+
+    l = parens_match_dc_helper(left)
+    r = parens_match_dc_helper(right)
+
+    return merge(l, r)
     """
     Recursive, divide and conquer solution to the parens match problem.
     
